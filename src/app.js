5830f83f0c590268081c16b615c7b9ee978aae16
Chadmichaelg <chadmgrubbs@gmail.com>
2022-04-06 22:07:40 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = domainGen();
+  });
+};
+let domainGen = () => {
+  let who = ["dog", "moon", "yomama", "vape"];
+  let action = ["bark", "attack", "ran", "ate"];
+  let what = [".com", ".org", ".net", ".edu"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+
+  return `${who[whoIndex]}${action[actionIndex]}${what[whatIndex]}`;
 };
